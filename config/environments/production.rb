54125b4e84bd442bda4a4c95253b548c8c1f9c2c
Elena Tolkacheva <hvs-fasya@mail.ru>
2017-07-16 21:08:16 +0300

    rm *.yml from git tracked files list first step

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,5 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { host: 'et-devise', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'et-devise' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = YAML.load(File.open("#{Rails.root}/config/smtp.yml"))
 end
