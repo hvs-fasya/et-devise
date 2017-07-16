ae65b5aefc32d1d7421f90910e460ebcecf0374c
Elena Tolkacheva <hvs-fasya@mail.ru>
2017-07-16 21:15:02 +0300

    add *.yml files to deployment linked files list

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :deploy_to, "/home/deployer/apps/et-devise"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/smtp.yml"
+append :linked_files, "config/database.yml", "config/smtp.yml", "config/cable.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
